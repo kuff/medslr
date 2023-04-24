ebabe193a165a3b113020279c8c4932802617ef9
kuff <16867891+kuff@users.noreply.github.com>
2023-04-24 14:32:20 +0200

    Add WIP hand gate refactor

--- Assets/Project/Runtime/Scripts/HandProvider.cs
+++ Assets/Project/Runtime/Scripts/HandProvider.cs
@@ -1,15 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class HandProvider : MonoBehaviour
 {
+    private Transform _headTransform;
     private OVRSkeleton _leftHand;
     private OVRSkeleton _rightHand;
     
     private OVRSkeleton _activeHand;
 
-    public void SeedHands(Transform _, OVRSkeleton leftHandSkeleton, OVRSkeleton rightHandSkeleton)
+    public void SeedHands(Transform headTransform, OVRSkeleton leftHandSkeleton, OVRSkeleton rightHandSkeleton)
     {
-        // We don't care about headTransform in this case...
+        _headTransform = headTransform;
         _leftHand = leftHandSkeleton;
         _rightHand = rightHandSkeleton;
 #if UNITY_EDITOR
@@ -40,5 +43,40 @@ public class HandProvider : MonoBehaviour
     public ref readonly OVRSkeleton GetActiveHand()
     {
         return ref _activeHand;
+    }
+
+    public bool isHandRaised(OVRHand.Hand hand)
+    {
+        //if (_headTransform == null) return false;
+
+        var handSkeleton = hand switch
+        {
+            OVRHand.Hand.HandLeft => _leftHand,
+            OVRHand.Hand.HandRight => _rightHand,
+            _ => null
+        };
+
+        // Check position of hand
+        var handTransform = handSkeleton.transform;
+        var delta = handTransform.position - _headTransform.position;
+        Debug.Log($"delta.normalized.z: {delta.normalized.z}");
+        if (delta.normalized.z < 0.1f) return false;
+
+        // Check position of fingers
+        var totalError = 0f;
+        for (var i = 0; i < handSkeleton.Bones.Count; i++)
+        {
+            var b = handSkeleton.Bones[i];
+
+            totalError += Mathf.Abs(b.Transform.position.x - TargetVectors.gate[i, 0]);
+            totalError += Mathf.Abs(b.Transform.position.y - TargetVectors.gate[i, 1]);
+            totalError += Mathf.Abs(b.Transform.position.z - TargetVectors.gate[i, 2]);
+
+            Debug.Log($"totalError: {totalError}");
+            if (totalError > 1f) return false;
+        }
+
+        // Both tests have passed
+        return true;
     }
 }
